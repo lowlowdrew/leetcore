471f0ef374176883775048297b59801fd1486930
XVVV <550575728@qq.com>
2023-09-30 14:23:41 +0800

    main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = { 2};
+        int n = 1111;
+        System.out.println(solution.getMaxNumber(nums, n));  // Output: 5288
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,2 +1,27 @@
+import java.util.Arrays;
+
 public class Solution {
+    int  max = 0;
+
+    public int getMaxNumber(int[] nums, int n) {
+          max = 0;
+          Arrays.sort(nums);
+          dfs(max,nums,n);
+
+        return max;
+    }
+
+    private void dfs(int current ,int[]nums, int n){
+        if(max>n){
+            return;
+        }
+        max=Math.max(max,current);
+        for(int i=nums.length-1;i>=0;i--){
+            int next=current*10+nums[i];
+            if(next<=n){
+                dfs(next,nums,n);
+            }
+        }
+
+    }
 }
